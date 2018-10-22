d28b21ded97ab3d7c374a287a5de6c1d6e58e81c
John Cusey <jjcusey@gmail.com>
2018-10-22 15:09:21 -0400

    Create MashrurNotes.rb

---
+++ MashrurNotes.rb
@@ -0,0 +1,38 @@
+print"--------------- STRINGS --------------- \n"
+
+# Determining Type
+
+put "".class
+
+put 1.class
+
+put false.class 
+
+# Find all methods
+
+put "".methods
+
+# Using the nil method
+
+put "".nil?
+put nil.nil?
+put false.nil?
+
+# Variables do not point to variables
+
+first_name = "John"
+second_name = first_name
+
+put second_name
+
+first_name = "Tim"
+
+put first_name
+
+# Escape Character
+
+put ' Hey Chris, John said \'How are your doing? \' ' 
+
+
+
+
