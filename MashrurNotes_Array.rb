c534384af64bb0a51fece711725524599e047881
John Cusey <jjcusey@gmail.com>
2018-10-24 12:52:50 -0400

    Update MashrurNotes_Array.rb

--- MashrurNotes_Array.rb
+++ MashrurNotes_Array.rb
@@ -2,9 +2,9 @@ def print_array_vertical(array)
    result = "[ "
    array.length.times do |i|
    result = result + array[i].to_s
-    if i != array.length - 1 
-      result = result + ", "
-    end
+      if i != array.length - 1 
+        result = result + ", "
+      end
    end
    result = result + "]"
    print result + "\n"
@@ -19,9 +19,11 @@ print "-- Shuffle of Array a -- \n"
 print_array_vertical( a.shuffle! )
 print "-- Create Array from 0 to 5 -- \n"
 print_array_vertical(  (0..5).to_a  )
+print "-- Pushing 77 on to the Array -- \n"
+print_array_vertical(  a << 77  )
+
 
 =begin
-
 -- Array a -- 
 [ 1, 2, 3, 4]
 -- Revere of Array a -- 
@@ -30,5 +32,6 @@ print_array_vertical(  (0..5).to_a  )
 [ 4, 1, 2, 3]
 -- Create Array from 0 to 5 -- 
 [ 0, 1, 2, 3, 4, 5]
-
+-- Pushing 77 on to the Array -- 
+[ 1, 2, 3, 4, 77]
 =end
