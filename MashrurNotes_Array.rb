d8878f92cf3043f0ad46501b867c9537046badaa
John Cusey <jjcusey@gmail.com>
2018-10-24 14:31:25 -0400

    Update MashrurNotes_Array.rb

--- MashrurNotes_Array.rb
+++ MashrurNotes_Array.rb
@@ -21,7 +21,14 @@ print "-- Create Array from 0 to 5 -- \n"
 print_array_vertical(  (0..5).to_a  )
 print "-- Pushing 77 on to the Array -- \n"
 print_array_vertical(  a << 77  )
-
+print "-- Pushing 3 on to the Array -- \n"
+print_array_vertical(  a.push( 3  ) )
+print "-- Pushing 'Mashrtrur' on to the Array -- \n"
+print_array_vertical(  a.unshift( "Mashrtrur"  ) )
+print "-- Show only unique values in the Array -- \n"
+print_array_vertical(  a.uniq )
+print "-- Create Array from 0 to 5 then random the order -- \n"
+y = 
 
 =begin
 -- Array a -- 
@@ -29,9 +36,15 @@ print_array_vertical(  a << 77  )
 -- Revere of Array a -- 
 [ 4, 3, 2, 1]
 -- Shuffle of Array a -- 
-[ 4, 1, 2, 3]
+[ 2, 4, 1, 3]
 -- Create Array from 0 to 5 -- 
 [ 0, 1, 2, 3, 4, 5]
 -- Pushing 77 on to the Array -- 
-[ 1, 2, 3, 4, 77]
+[ 2, 4, 1, 3, 77]
+-- Pushing 3 on to the Array -- 
+[ 2, 4, 1, 3, 77, 3]
+-- Pushing 'Mashrtrur' on to the Array -- 
+[ Mashrtrur, 2, 4, 1, 3, 77, 3]
+-- Show only unique values in the Array -- 
+[ Mashrtrur, 2, 4, 1, 3, 77]
 =end
