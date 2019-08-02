3ab2ed9ccac1816c09a18d075abd7e8817e155ee
John Cusey <jjcusey@gmail.com>
2019-08-02 14:59:04 -0400

    Update workspace.rb

--- workspace.rb
+++ workspace.rb
@@ -1,3 +1,4 @@
+
 # ARRAYS
 z = [1,2,3]
 
@@ -21,5 +22,25 @@ puts my_types['gender']
 my_x = { :symbol => 'cow'}
 my_hash = {:cool => :symbol}
 puts my_x [ my_hash[ :cool ] ]
+
+puts "-"*10 #----------
+
+# class
+
+class Student
+  @first_name
+
+  def first_name=(name)
+    @first_name = name
+  end
+
+  def to_s
+    "First Name: #{@first_name}"
+  end
+end
+
+john = Student.new
+john.first_name = "John"
+puts john
 
 puts "-"*10 #----------
